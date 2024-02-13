f66f65d8a8f531def9d6dbdf9c077790347bfd5e
Lucas Souza <145349067+llslucas115@users.noreply.github.com>
2024-02-13 13:37:12 -0300

    Limitação de Height removida

--- Stage 09 - Desafio/rocketmovies/src/components/Header/styles.js
+++ Stage 09 - Desafio/rocketmovies/src/components/Header/styles.js
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 export const Container = styled.div`
     grid-area: 'header';
     width: 100%;
-    height: 11.6rem;
     display: flex;
     flex-direction: row;
 
