051164841ff4d77c6067bc3085cad99120e95de1
Lucas Souza <145349067+llslucas115@users.noreply.github.com>
2024-02-13 18:01:44 -0300

    Rota para a página Preview Criada

--- Stage 09 - Desafio/rocketmovies/src/routes/app.routes.jsx
+++ Stage 09 - Desafio/rocketmovies/src/routes/app.routes.jsx
@@ -3,13 +3,15 @@ import { Routes, Route } from 'react-router-dom';
 import { Home } from '../pages/Home';
 import { CreateMovie } from '../pages/CreateMovie';
 import { Profile } from '../pages/Profile';
+import { Preview } from '../pages/Preview';
 
 export function AppRoutes(){
     return(
         <Routes>        
             <Route path='/' element={<Home />} />
             <Route path='/create' element={<CreateMovie/>} />
-            <Route path='/Profile' element={<Profile />} />
+            <Route path='/profile' element={<Profile />} />
+            <Route path='/preview/:id' element={<Preview />} />
         </Routes>
     )
 }
