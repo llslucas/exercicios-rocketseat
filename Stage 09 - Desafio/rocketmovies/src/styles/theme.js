588cda20c6df4a0a511b6fa24f3b7af951be147e
Lucas Souza <145349067+llslucas115@users.noreply.github.com>
2024-02-13 11:16:16 -0300

    Estilos Globais Criados

---
+++ Stage 09 - Desafio/rocketmovies/src/styles/theme.js
@@ -0,0 +1,16 @@
+export default {
+    COLORS: {      
+      BACKGROUND_900: "#1C1B1E",
+      BACKGROUND_800: "#262529",
+      BACKGROUND_700: "#312E38",
+  
+      WHITE: "#F4EDE8",
+      ROSE: "#FF859B",
+  
+      GRAY_100: "#E5E5E5",
+      GRAY_200: "#CAC4CF",
+      GRAY_300: "#948F99",
+
+      BLACK: "#0D0C0F"
+    }
+  }
