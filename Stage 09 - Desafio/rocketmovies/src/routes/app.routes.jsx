6d2c73b28d204a2da1bed943fd30624ff470fe9a
Lucas Souza <145349067+llslucas115@users.noreply.github.com>
2024-02-13 15:24:51 -0300

    Home adicionada às Routes

--- Stage 09 - Desafio/rocketmovies/src/routes/app.routes.jsx
+++ Stage 09 - Desafio/rocketmovies/src/routes/app.routes.jsx
@@ -1,9 +1,11 @@
 import { Routes, Route } from 'react-router-dom'
 
+import { Home } from '../pages/Home'
 
 export function AppRoutes(){
     return(
-        <Routes>           
+        <Routes>        
+            <Route path='/' element={<Home />} />
         </Routes>
     )
 }
