485beae5912920453cd8aa69b498cc9223646ad0
Lucas Souza <145349067+llslucas115@users.noreply.github.com>
2024-02-13 16:49:48 -0300

    Componente TextArea Criado

---
+++ Stage 09 - Desafio/rocketmovies/src/components/TextArea/styles.js
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
+    background-color: ${({ theme }) => theme.COLORS.BACKGROUND_800};    
+    padding: 1.8rem;
+    border-radius: 1rem;
+
+    > textarea {
+        font-size: 1.6rem;        
+        background: transparent;
+        width: 100%;
+        height: 20rem;
+        border: none;        
+        color: ${({ theme }) => theme.COLORS.WHITE};
+
+        resize: none;
+
+        &:focus{
+            outline: none;
+        }
+
+        &::placeholder{
+            color: ${({ theme }) => theme.COLORS.GRAY_300};
+        }
+    }
+
+    > svg {
+        color: ${({ theme }) => theme.COLORS.GRAY_300};
+        width: 1.8rem;
+        height: 1.8rem;
+        margin-right: 18px;
+    }
+`
