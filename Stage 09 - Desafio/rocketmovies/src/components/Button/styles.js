28bbe502d8c8cd5d3c853b009e1eaf9f020a3305
Lucas Souza <145349067+llslucas115@users.noreply.github.com>
2024-02-13 12:13:17 -0300

    Estilos modificados

--- Stage 09 - Desafio/rocketmovies/src/components/Button/styles.js
+++ Stage 09 - Desafio/rocketmovies/src/components/Button/styles.js
@@ -2,11 +2,21 @@ import styled from 'styled-components';
 
 export const Container =  styled.button`
     width: 100%;
-    padding: 0 1.68rem;
+    padding: 1.68rem 0;
 
     background-color: ${({ theme }) => theme.COLORS.ROSE};
     color: ${({ theme }) => theme.COLORS.BACKGROUND_700};
+    border: none;
+    border-radius: 1rem;
     
     font-size: 1.6rem;    
     text-align: center;
+
+    cursor: pointer;
+
+    transition-duration: 0.2s;
+
+    &:hover{
+        filter: brightness(1.2);
+    }
 `
