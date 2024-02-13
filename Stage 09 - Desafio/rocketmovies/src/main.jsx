88e79d23349ce29d35279ac8ce74be4f40251eb8
Lucas Souza <145349067+llslucas115@users.noreply.github.com>
2024-02-13 12:15:44 -0300

    Rotas Adicionadas

--- Stage 09 - Desafio/rocketmovies/src/main.jsx
+++ Stage 09 - Desafio/rocketmovies/src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './styles/global';
 import theme from './styles/theme';
+import { Routes } from './routes';
+
+import { SignIn } from './pages/SignIn';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
+      <Routes />
     </ThemeProvider>    
   </React.StrictMode>,
 )
