3ad93b904e02959639442c0d804120cbf1a05af8
Lucas Souza <145349067+llslucas115@users.noreply.github.com>
2024-02-13 16:51:11 -0300

    Adicionados Estilos globais para button e input

--- Stage 09 - Desafio/rocketmovies/src/styles/global.js
+++ Stage 09 - Desafio/rocketmovies/src/styles/global.js
@@ -5,25 +5,45 @@ export default createGlobalStyle`
         margin: 0;
         padding: 0;
         box-sizing: border-box;        
-    }
+    };
 
     :root{
         font-size: 62.5%; 
-    }
+    };
 
     body{
         background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
         color: ${({ theme }) => theme.COLORS.WHITE};
 
         -webkit-font-smoothing: antialiased;
-    }
+    };
 
     body, input, button, textarea {
         font-family: 'Roboto Slab', serif;
-    }
+    };
 
     a{
         text-decoration: none;    
         color: ${({ theme }) => theme.COLORS.WHITE};    
+    };
+
+    button{
+        background: none;
+        border: none;
+        cursor: pointer;
+
+        display: flex;
+        align-items: center;
+    };
+
+    input{
+        color: ${({ theme }) => theme.COLORS.WHITE};  
+        background: none;
+        border: none;
+        font-size: inherit;
+    };
+
+    input:focus{
+        outline: none;
     }
 `;
