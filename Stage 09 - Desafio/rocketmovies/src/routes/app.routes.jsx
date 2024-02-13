2cdd9cd0cbef858a742ac1c22b2cce4d2c72d257
Lucas Souza <145349067+llslucas115@users.noreply.github.com>
2024-02-13 16:50:38 -0300

    Rotas para CreateMovie e Profile adicionadas

--- Stage 09 - Desafio/rocketmovies/src/routes/app.routes.jsx
+++ Stage 09 - Desafio/rocketmovies/src/routes/app.routes.jsx
@@ -1,11 +1,15 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom';
 
-import { Home } from '../pages/Home'
+import { Home } from '../pages/Home';
+import { CreateMovie } from '../pages/CreateMovie';
+import { Profile } from '../pages/Profile';
 
 export function AppRoutes(){
     return(
         <Routes>        
             <Route path='/' element={<Home />} />
+            <Route path='/create' element={<CreateMovie/>} />
+            <Route path='/Profile' element={<Profile />} />
         </Routes>
     )
 }
