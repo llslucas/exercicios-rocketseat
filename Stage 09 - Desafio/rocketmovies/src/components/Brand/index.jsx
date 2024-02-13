f1db0147bd1a92c0469544377ffe2cec0eea9cc5
Lucas Souza <145349067+llslucas115@users.noreply.github.com>
2024-02-13 17:59:35 -0300

    Link para a Home Adicionado

--- Stage 09 - Desafio/rocketmovies/src/components/Brand/index.jsx
+++ Stage 09 - Desafio/rocketmovies/src/components/Brand/index.jsx
@@ -1,9 +1,14 @@
 import { Container } from "./styles";
+import { Link } from "react-router-dom";
 
 export function Brand({ size=1, subtitle=false }){
     return (
         <Container $size={size.toString()} >
-            <h1>RocketMovies</h1>
+            <h1>
+                <Link to='/'>
+                    RocketMovies
+                </Link>
+            </h1>
             {subtitle && <h2>Aplicação para acompanhar tudo que assistir.</h2>}
         </Container>
     )
