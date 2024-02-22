8fa1684a169cfda967fdbd8eb39018cab8fe9d7d
Lucas Souza <145349067+llslucas115@users.noreply.github.com>
2024-02-21 22:25:24 -0300

    Middleware ensureAuthenticated criado

--- Stage 10 - Integração/src/routes/users.routes.js
+++ Stage 10 - Integração/src/routes/users.routes.js
@@ -1,10 +1,11 @@
 import Router from 'express';
 import UsersController from '../controllers/UsersController.js';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated.js'
 
 const usersRouter = Router();
 const usersController = new UsersController();
 
 usersRouter.post("/", usersController.create);
-usersRouter.put("/:id", usersController.update);
+usersRouter.put("/", ensureAuthenticated, usersController.update);
 
 export default usersRouter;
