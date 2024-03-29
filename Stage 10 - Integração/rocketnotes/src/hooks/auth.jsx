4f684e8de5e1a473aefb1a5c6e9dd5423ed0b73c
Lucas Souza <145349067+llslucas115@users.noreply.github.com>
2024-03-29 19:41:54 -0300

    Adicionar persistência de login pelo LocalStorage

--- Stage 10 - Integração/rocketnotes/src/hooks/auth.jsx
+++ Stage 10 - Integração/rocketnotes/src/hooks/auth.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 
 import { api } from '../services/api';
 
@@ -7,11 +7,13 @@ const AuthContext = createContext({});
 export function AuthProvider({ children }){
     const [data, setData] = useState({});
 
-
     async function signIn({ email, password }){
         try{
             const response = await api.post("/sessions", { email, password });
             const {user, token} = response.data;
+
+            localStorage.setItem("@rocketnotes:user", JSON.stringify(user));
+            localStorage.setItem("@rocketnotes:token", token);
 
             api.defaults.headers.authorization = `Bearer ${token}`;
             setData({user, token});
@@ -26,6 +28,19 @@ export function AuthProvider({ children }){
             }
         }        
     }
+
+    useEffect( () => {
+        const token = localStorage.getItem("@rocketnotes:token");
+        const user = localStorage.getItem("@rocketnotes:user");
+
+        if(token && user){
+            api.defaults.headers.authorization = `Bearer ${token}`;
+            setData({
+                token,
+                user: JSON.parse(user)                
+            });
+        }
+    }, []);
 
     return (
         <AuthContext.Provider value={{ signIn, user: data.user }}>
