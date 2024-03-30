cebb35a12156ea3d52a7d0489fae6f91fffb21c6
Lucas Souza <145349067+llslucas115@users.noreply.github.com>
2024-03-29 21:27:12 -0300

    Função de Signout criada

--- Stage 10 - Integração/rocketnotes/src/hooks/auth.jsx
+++ Stage 10 - Integração/rocketnotes/src/hooks/auth.jsx
@@ -29,6 +29,13 @@ export function AuthProvider({ children }){
         }        
     }
 
+    function signOut(){
+        localStorage.removeItem("@rocketnotes:token");
+        localStorage.removeItem("@rocketnotes:user");
+
+        setData({});
+    }
+
     useEffect( () => {
         const token = localStorage.getItem("@rocketnotes:token");
         const user = localStorage.getItem("@rocketnotes:user");
@@ -43,7 +50,11 @@ export function AuthProvider({ children }){
     }, []);
 
     return (
-        <AuthContext.Provider value={{ signIn, user: data.user }}>
+        <AuthContext.Provider value={{ 
+            signIn, 
+            user: data.user,
+            signOut
+        }}>
             {children}
         </AuthContext.Provider>
     )
